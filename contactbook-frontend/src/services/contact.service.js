bba6c23f06f28bc2960c16dff6eddf103bd3d9de
Vo <vob2014721@student.ctu.edu.vn>
2023-03-06 11:16:02 +0700

    tao lop dich vu lay du lieu tu server

---
+++ contactbook-frontend/src/services/contact.service.js
@@ -0,0 +1,42 @@
+import contactAPIClient from "./api.service";
+
+class ContactService {
+    constructor(baseURL = "/api/contacts") {
+        this.api = contactAPIClient(baseURL);
+    }
+
+    async getAll(name = {}) {
+        return (await this.api.get(`/${name}`)).data
+    }
+
+    async create(data) {
+        return (await this.api.post("/", data)).data
+    }
+
+    async deleteAll() {
+        return (await this.api.detele("/")).data
+    }
+
+    async updateAll(data) {
+        return (await this.api.put('/', data)).data
+    }
+
+
+    async getAllFavorite() {
+        return (await this.api.get('/favorite')).data
+    }
+
+    async get(id) {
+        return (await this.api.post(`/${id}`)).data
+    }
+
+    async update(id, data) {
+        return (await this.api.put(`/${id}`, data)).data
+    }
+
+    async delete (id) {
+        return (await this.api.delete(`${id}`)).data
+    }
+}
+
+export default new ContactService();
