63110a6ed03ef11129f6fc547142f80ebdd8ada6
Vo <vob2014721@student.ctu.edu.vn>
2023-03-23 11:36:14 +0700

    Hien thi danh sach cac contact lay ve tu server

---
+++ contactbook-frontend/src/components/AppHeader.vue
@@ -0,0 +1,20 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a href="/" class="navbar-brand">Ứng dụng quản lý danh bạ</a>
+        <div class="collapse navbar-collapse">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <router-link :to="{name: 'Contactbook'}" class="nav-link">
+                        Danh bạ
+                        <i class="fas fa-address-book"></i>
+                    </router-link>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</template>
+<style scoped>
+    nav{
+        margin-bottom: 16px;
+    }
+</style>
---
+++ contactbook-frontend/src/components/InputSearch.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="input-group">
+        <input type="text" 
+               class="form-control" 
+               placeholder="Nhập thông tin cần tìm" 
+               :value="modelValue" 
+               @input="updateModelValue" 
+               @keypress.enter="submit"
+               name="" id="">
+
+        <div class="input-group-append">
+            <button class="btn btn-outline-secondary" type="button" @click="submit">
+                <i class="fa-solid fa-magnifying-glass"></i>Tìm kiếm  
+            </button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default{
+    props: {
+        modelValue: {
+            type:String, default: ""
+        },
+    },
+    emits: ["submit", "update:modelValue"],
+    methods: {
+        updateModelValue(e) {
+            this.$emit("update:modelValue", e.target.value);
+        },
+        submit(e) {
+            this.$emit("submit");
+        }
+
+    }
+}
+</script>
+
+<style>
+</style>
